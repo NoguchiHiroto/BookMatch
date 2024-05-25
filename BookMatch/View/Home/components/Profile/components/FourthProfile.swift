c74fe0d861819484116fe2ccab30c8101d82f4fd
hiroto_noguchi <noguchi.hiroto@holon-inc.jp>
2024-05-26 01:26:05 +0900

    add: 4つ目のプロフィール画面の文言を追加[profile]

---
+++ BookMatch/View/Home/components/Profile/components/FourthProfile.swift
@@ -0,0 +1,18 @@
+//
+//  FourthProfile.swift
+//  BookMatch
+//
+//  Created by noguchi.hiroto on 2024/05/25.
+//
+
+import SwiftUI
+
+struct FourthProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FourthProfile()
+}
